6998bf504fa97d6025fda166580feec53a62eead
estherakinn <53815387+estherakinn@users.noreply.github.com>
2019-08-08 15:30:12 +0100

    Added a page2
    and made a button

---
+++ javascript/interaction.js
@@ -0,0 +1,8 @@
+
+$( "#split" ).click(function() {
+ console.log('hi')
+ // $('#split').css('color', '#E91179')
+ // similar behavior as an HTTP redirect
+ window.location.replace("./page2.html");
+
+});
